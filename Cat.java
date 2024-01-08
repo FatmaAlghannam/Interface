2c3e1ed5cc8a4bdd2b465b44ab35fbb29a53aa0f
Fatimah A F A M Alghannam <83319@kfh.com>
2024-01-08 11:10:53 +0300

    Task-Interface-Kingdom

---
+++ Cat.java
@@ -0,0 +1,17 @@
+package Kingdom;
+
+public class Cat implements Animal {
+
+    public void eat() {
+        System.out.println("Cats eats salmon");
+    }
+
+    public void sleep() {
+        System.out.println("Cats are sleepy ");
+    }
+
+    public void makeSound() {
+        System.out.println("Cats sounds meow!!!");
+    }
+
+}
---
+++ Dog.java
@@ -0,0 +1,17 @@
+package Kingdom;
+
+public class Dog implements Animal {
+
+    public void eat() {
+        System.out.println("Dogs eats meat");
+    }
+
+    public void sleep() {
+        System.out.println("Dogs are lazy ");
+    }
+
+    public void makeSound() {
+        System.out.println("Dogs sounds Whouff..Whouff!!!");
+    }
+
+}
